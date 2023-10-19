5b4cd61b4a5cea3ecac221568f3a3af211f7abbd
ryanhaddad <ryanhaddadpro@gmail.com>
2023-10-19 21:26:58 +0200

    create file and folder need to payapl payment

--- paypal.php
+++ paypal.php
@@ -11,9 +11,80 @@
 </head>
 
 <body>
+    <script src="https://ww.paypal.com/sdk/js?client-id=Ac2Qlp2PmOKd4eicwlZ1VvwYBojOONCONEKoKJB9iNIj4AQH4KBFOvRr58ZPlsbsb2AW3oGMM5EtXoFf&currency=USD"></script>
     <section>
+        <div class="panel">
+            <div class="overlay hidden"><div class="overlay-content"><img src="css/loading.gif" alt="Processing..."/></div></div>
 
-        <div class="formulaire">
+            <div class="panel-heading">
+                <h3 class="panel-title">Charge <?php ?> with PayPal</h3>
+                
+                <!-- Product Info -->
+                <p><b>Item Name:</b> <?php  ?></p>
+                <p><b>Price:</b> <?php  ?></p>
+            </div>
+            <div class="panel-body">
+                <!-- Display status message -->
+                <div id="paymentResponse" class="hidden"></div>
+                
+                <!-- Set up a container element for the button -->
+                <div id="paypal-button-container"></div>
+            </div>
+        </div>
+        <script>
+            paypal.Buttons({
+                createOrder: (data,action) => {
+                    return action.order.create({
+                        "purchase_units": [{
+                            "custom_id":"<?php echo "12" ?>",
+                            "description":"<?php echo "testItemName" ?>",
+                            "amount": {
+                                "currency_code":"<?php  ?>",
+                                "value": "<?php  ?>",
+                                "breakdown": {
+                                    "item_total": {
+                                        "currency_code":"<?php ?>",
+                                        "value": "<?php ?>"
+                                    }
+                                }
+                            },
+                            "items":[
+                                {
+                                    "name": "<?php  ?>",
+                                    "description": "<?php  ?>",
+                                    "unit_amount":{
+                                        "currency_code":"<?php  ?>",
+                                        "value":"<?php  ?>"
+                                    },
+                                    "quantity": 1,
+                                    "category":"DIGITAL_GOODS"
+                                }
+                            ]
+                        }]
+                    })
+                },
+                onApprove: (date, action ) => {
+                    return action.order.capture().then(function(orderData){
+
+                    });
+                }
+
+            }).render('#paypal-button-container');
+
+            const encodeFormData = (data) => {
+                var form_data = new FormData();
+
+                for(var key in data ){
+                    form_data.append(key,data[key]);
+                }
+                return form_data;
+            }
+
+
+          
+        </script>
+
+        <!-- <div class="formulaire">
 
             <form class="form" action="./back/check_paypal.php" method="POST">
                 <div class="menu-form">
@@ -39,7 +110,7 @@
                 </div>
             </form>
         </div>
-        </div>
+        </div> -->
 
     </section>
     <?php
